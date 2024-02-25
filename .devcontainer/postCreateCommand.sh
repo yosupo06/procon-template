2ba759260b8ef57b62f28f56fd01a47bb96db921
Kohei Morita <yosupo06@gmail.com>
2024-02-25 23:30:14 +0900

    initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update
+sudo apt-get install -y sqlite3 xsel
+
+pip install psytester online-judge-tools online-judge-verify-helper
+
+git clone https://github.com/atcoder/ac-library $HOME/ac-library
+git clone https://github.com/yosupo06/yosupo-library $HOME/yosupo-library
+git clone https://github.com/yosupo06/Algorithm $HOME/Algorithm
+git clone https://github.com/yosupo06/fastfps $HOME/fastfps
+
+go install github.com/yosupo06/supporter@latest
