9f4d3bb5a60f3fe4ee6a7cc7a61961753eec50c2
Kohei Morita <yosupo06@gmail.com>
2024-04-18 00:56:39 +0900

    add cargo generate

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,6 +7,8 @@ sudo apt-get install -y sqlite3 xsel
 
 pip install psytester online-judge-tools online-judge-verify-helper ipykernel
 
+cargo install cargo-generate
+
 git clone https://github.com/atcoder/ac-library $HOME/ac-library
 git clone https://github.com/yosupo06/yosupo-library $HOME/yosupo-library
 git clone https://github.com/yosupo06/Algorithm $HOME/Algorithm
