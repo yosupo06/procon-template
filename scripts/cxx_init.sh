8a90466bc894e4cf8acdacbf6147045b66d28ded
Kohei Morita <yosupo06@gmail.com>
2024-06-02 03:42:07 +0000

    upd template

--- scripts/cxx_init.sh
+++ scripts/cxx_init.sh
@@ -7,7 +7,6 @@ template_dir=$(realpath $(dirname $(realpath $0))/../template/cxx)
 for target_dir in "$@"; do
     if [ ! -d "$target_dir" ]; then
         echo "[init] Creating directory $target_dir"
-        mkdir -p "$target_dir"
         cp -r $template_dir $target_dir
     else
         echo "[init] $target_dir already exists, skip"
