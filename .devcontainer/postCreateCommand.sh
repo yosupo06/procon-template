0e3611a4d9aaf7877a9e5f58beaf2693b59e9916
Kohei Morita <yosupo06@gmail.com>
2024-06-02 13:42:48 +0900

    add

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -10,16 +10,14 @@ wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
 chmod +x /tmp/llvm.sh
 sudo /tmp/llvm.sh 18
 
-sudo apt-get install -y sqlite3 xsel
-
 # oj
 pip install online-judge-tools online-judge-verify-helper
 
+# marathon
 pip install psytester ipykernel
 
+# library
 git clone https://github.com/atcoder/ac-library $HOME/ac-library
 git clone https://github.com/yosupo06/yosupo-library $HOME/yosupo-library
 git clone https://github.com/yosupo06/Algorithm $HOME/Algorithm
 git clone https://github.com/yosupo06/fastfps $HOME/fastfps
-
-go install github.com/yosupo06/supporter@latest
